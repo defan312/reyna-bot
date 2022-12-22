b2124abf4136ff9c95c7c7b55491d7b2bd3d2bdc
defan312 <106823339+defan312@users.noreply.github.com>
2022-12-22 20:02:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+global.bot='ᴬᴺᴵᴬ ᴮᴼᵀ'
+global.bot2='「 ᴬᴺᴵᴬ ᴮᴼᵀ 」'
+global.bot3='🌱 〣 ᴬᴺᴵᴬ ᴮᴼᵀ'
+global.botname='ᴬᴺᴵᴬ'
+global.botnumber='6281321776496'
+
+global.ownername='ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×'
+global.ownerig='ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×'
+global.ig='https://www.instagram.com/defan_sugiarto'
+global.wagroup='https://chat.whatsapp.com/I9gCWSLGlmjDFLG3WC9RvW'
+global.ownernumber='6281321776496'
+
+global.wm='2022 © ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×'
+global.wm2='Created By ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×'
